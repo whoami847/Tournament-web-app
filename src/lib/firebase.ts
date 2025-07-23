01fc34c52c86056901a1fa6302fd640822b37ecc
whoami847 <mrbeastbangla069@gmail.com>
2025-07-23 20:40:44 +0600

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDReCAxTGjgho2rWUQKCwU8Z3aoB0ZiYN4",
-  authDomain: "aff-tour-1-0.firebaseapp.com",
-  projectId: "aff-tour-1-0",
-  storageBucket: "aff-tour-1-0.firebasestorage.app",
-  messagingSenderId: "362331001350",
-  appId: "1:362331001350:web:e756adec103071c849e987"
+  apiKey: "AIzaSyDu_sHRlv2UwAGmnpLBv5dYRUwzoG_im98",
+  authDomain: "aff-tournament.firebaseapp.com",
+  projectId: "aff-tournament",
+  storageBucket: "aff-tournament.firebasestorage.app",
+  messagingSenderId: "180328926458",
+  appId: "1:180328926458:web:fdfcda4a074651a1decd43"
 };
 
 let app;
